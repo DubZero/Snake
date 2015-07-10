b3fbe7b58a47919ab95b11af351c94dd005b8e12
DubZero <shumkinalexandrne@gmail.com>
2015-07-10 11:45:38 +0300

    Реализован перемещение змейки а так же поворот

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -3,13 +3,38 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+    public GameObject snake;
+
+    public float SpawnTime = 0.5f;
+
+
 	// Use this for initialization
 	void Start () {
-	
+        Invoke("MoveSnake",SpawnTime);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        
 	}
+
+    void MoveSnake()
+    {
+        
+     
+        Invoke("MoveSnake", SpawnTime);
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            print("Left нажато");
+            transform.Rotate(new Vector3(0,0, 90));
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            print("Right нажато");
+            transform.Rotate(new Vector3(0, 0,-90));
+        }
+        transform.position += transform.up;
+    }
+
+    
 }
