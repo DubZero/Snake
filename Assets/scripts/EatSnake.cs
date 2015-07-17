59704cf680326510a149bf88ec462d7dd9d3013b
DubZero <shumkinalexandrne@gmail.com>
2015-07-17 15:47:42 +0300

    Коммит перед созданием ветки

--- Assets/scripts/EatSnake.cs
+++ Assets/scripts/EatSnake.cs
@@ -2,14 +2,105 @@
 using System.Collections;
 
 public class EatSnake : MonoBehaviour {
-
+    public float SpawnTime = 0.5f;   // частота перемещения змейки
 	// Use this for initialization
 	void Start () {
-	
+        Invoke("MoveSnake", SpawnTime);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+    void MoveSnake()
+    {
+        Invoke("MoveSnake", SpawnTime);
+
+        // Управление змеей
+        if (transform.eulerAngles.z == 0)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                print("Left нажато");
+                transform.Rotate(new Vector3(0, 0, 90));
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                print("Right нажато");
+                transform.Rotate(new Vector3(0, 0, -90));
+            }
+        }
+        else if (transform.eulerAngles.z == 180)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                print("Left нажато");
+                transform.Rotate(new Vector3(0, 0, -90));
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                print("Right нажато");
+                transform.Rotate(new Vector3(0, 0, 90));
+            }
+        }
+        else if (transform.eulerAngles.z == 270)
+        {
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                print("Down нажато");
+                transform.Rotate(new Vector3(0, 0, -90));
+            }
+            else if (Input.GetKey(KeyCode.UpArrow))
+            {
+                print("Up нажато");
+                transform.Rotate(new Vector3(0, 0, 90));
+            }
+        }
+        else if (transform.eulerAngles.z == 90.00001f)
+        {
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                print("Down нажато");
+                transform.Rotate(new Vector3(0, 0, 90));
+            }
+            else if (Input.GetKey(KeyCode.UpArrow))
+            {
+                print("Up нажато");
+                transform.Rotate(new Vector3(0, 0, -90));
+            }
+        }
+
+        //=========================
+        // Рост змейки
+        //=========================
+        Vector2 ta = transform.position;
+
+        // Если True то увеличение размера на 1
+        if (eat)
+        {
+            // Загрузка префаба хвоста
+            tail = Instantiate(Resources.Load("Tail"), ta, Quaternion.identity) as GameObject;
+            // Сохранение пути в лист 
+            listTail.Insert(0, tail.transform);
+
+            // Обнуление флага
+            eat = false;
+        }
+        // Есть ли объекти хвоста?
+        else if (listTail.Count > 0)
+        {
+            // Перемещение элемента хвоста на последнюю позицию где была голова 
+            listTail.Last().position = ta;
+
+            // Добавление в начало листа, и очистка с конца
+            listTail.Insert(0, listTail.Last());
+            listTail.RemoveAt(listTail.Count - 1);
+        }
+
+        // Перемещение на transform.up;
+        if (StopFlag == false)
+        {
+            transform.position += transform.up;
+        }
+    }
 }
--- Assets/scripts/Spawn.cs
+++ Assets/scripts/Spawn.cs
@@ -2,8 +2,14 @@
 using System.Collections;
 
 public class Spawn : MonoBehaviour {
+    public GameObject food; // объект еда
+    public GameObject tail;// объект хвост
+    public GameObject LeftWall;// объект Левая граница
+    public GameObject UpWall;// объект Верхняя граница
+    public GameObject RightWall;// объект Правая граница
+    public GameObject DownWall;// объект Нижняя граница
+	
 
-	// Use this for initialization
 	void Start () {
 	
 	}
