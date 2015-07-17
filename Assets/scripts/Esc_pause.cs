c75e2b24a8bc3d96e17b32948c4c7f9262a45ada
DubZero <shumkinalexandrne@gmail.com>
2015-07-17 14:07:27 +0300

    Добавление Esc меню скрипта

--- Assets/scripts/Esc_pause.cs
+++ Assets/scripts/Esc_pause.cs
@@ -3,13 +3,35 @@ using System.Collections;
 
 public class Esc_pause : MonoBehaviour {
 
-	// Use this for initialization
+
+    public bool PauseFlag = false;
+    public GameObject Head;
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (PauseFlag == true)
+            {
+                PauseFlag = false;
+            }
+            else
+            {
+                PauseFlag = true;
+            }
+        }
 	}
+    void OnGUI()
+    {
+        if (PauseFlag)
+        {
+            
+            GUI.Button(new Rect((Screen.width - 200) / 2, (Screen.height + 60) / 2, 200, 30), "Начать заного");
+            GUI.Button(new Rect((Screen.width - 200) / 2, (Screen.height + 150) / 2, 200, 30), "Выход");
+           
+        }
+
+        
+    }
 }
