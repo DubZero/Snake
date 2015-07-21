0e1f2166dda60cb9cb01b9f2b7a649010cb32188
DubZero <shumkinalexandrne@gmail.com>
2015-07-21 19:41:29 +0300

    Испорченная версия

---
+++ Assets/scripts/GOScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class GOScoreManager : MonoBehaviour {
+
+
+    Text text;
+    public int score;
+    public GameObject GOScoreText;
+    void Awake()
+    {
+        text = GetComponent<Text>();
+        score = 0;
+        GOScoreText.SetActive(false);
+    }
+	
+	
+	void Update () {
+        
+        if (GameObject.Find("Head").GetComponent<GameOver>().GameOverFlag)
+        {
+            score = GameObject.Find("ScoreText").GetComponent<ScoreManager>().score;
+            text.text = "Score: " + score;
+            GOScoreText.SetActive(true);
+        }
+	}
+}
--- Assets/scripts/ScoreManager.cs
+++ Assets/scripts/ScoreManager.cs
@@ -6,13 +6,12 @@ public class ScoreManager : MonoBehaviour {
 
     Text text;
     public int score;
-
     void Awake()
     { 
         text = GetComponent<Text>();
         score = 0;
     }
-    void Update()
+     void Update()
     {
         // Обновление счетчика очков
         text.text = "Score: " + score;
